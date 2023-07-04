5b2a701641b5310e9ec688242343a2c62745b32d
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2023-07-04 14:22:59 +0900

    0704 - Magazine 관련 컨트롤러 수정

--- project-1/src/main/java/com/kosmo/springapp/controller/MagazineController.java
+++ project-1/src/main/java/com/kosmo/springapp/controller/MagazineController.java
@@ -3,18 +3,26 @@ package com.kosmo.springapp.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kosmo.springapp.model.FunctionalFoodListDTO;
+import com.kosmo.springapp.service.impl.MainPageServiceImpl;
+
 
 @Controller
 public class MagazineController {
+	
+	@Autowired
+	MainPageServiceImpl mainPageService;
+	
 	@GetMapping("/magazine.do")
 	  public String magazine(@RequestParam String no,Map map,Model model) {
-	    //List<FunctionalFoodListDTO> listData = mainService.selectFoodList(map);
-	    //model.addAttribute("listData",listData);
+	    List<FunctionalFoodListDTO> listData = mainPageService.selectFoodList();
+	    model.addAttribute("listData",listData);
 	    return "slider/Magazine"+no;
 	  }
 }
