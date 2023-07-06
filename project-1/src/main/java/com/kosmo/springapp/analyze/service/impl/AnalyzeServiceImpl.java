8e8bd9c5d72c3269e4ee4ddd2d9a7f1aa5e8dd3f
Kimtaehyun <xogus3308@naver.com>
2023-07-06 16:26:43 +0900

    07-06 config에 추가 및 영양소DTO수정 xml추가

--- project-1/src/main/java/com/kosmo/springapp/analyze/service/impl/AnalyzeServiceImpl.java
+++ project-1/src/main/java/com/kosmo/springapp/analyze/service/impl/AnalyzeServiceImpl.java
@@ -1,9 +1,20 @@
 package com.kosmo.springapp.analyze.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AnalyzeServiceImpl {
+import com.kosmo.springapp.analyze.model.NutrientDTO;
+import com.kosmo.springapp.analyze.service.AnalyzeMapper;
+import com.kosmo.springapp.analyze.service.AnalyzeService;
 
+@Service
+public class AnalyzeServiceImpl implements AnalyzeService {
+
+	@Autowired
+	AnalyzeMapper mapper;
+	
+	public NutrientDTO findNutrientByName(String str) {
+		return mapper.findNutrientByName(str);
+	}
 	
 }
