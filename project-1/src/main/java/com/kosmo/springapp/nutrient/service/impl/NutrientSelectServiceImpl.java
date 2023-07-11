caf03983797b8ef8d12563a21ee260b34733c812
hjjsimon <hjjsimon@gmail.com>
2023-07-11 18:16:33 +0900

    0711 - 세부사항 및 진행바 완료
    세부사항 및 진행바 완료

--- project-1/src/main/java/com/kosmo/springapp/nutrient/service/impl/NutrientSelectServiceImpl.java
+++ project-1/src/main/java/com/kosmo/springapp/nutrient/service/impl/NutrientSelectServiceImpl.java
@@ -104,7 +104,59 @@ public class NutrientSelectServiceImpl implements NutrientSelectService {
 			desc = desc.replace("만약 고용량을 드시려는 경우에는", "· 만약 고용량을 드시려는 경우에는");
 			desc = desc.replace("1) 뼈 & 관절 건강: 0.5~1(g)","1) 뼈 & 관절 건강: 0.5~1(g)<br/><br/>");
 			desc = desc.replace("HCA 함량이 표시되어 있다면", "· HCA 함량이 표시되어 있다면");
-			
+			desc = desc.replace("2) 혈행 개선:", "<br/><br/> 2) 혈행 개선:");
+			desc = desc.replace("3) PMS 개선:", "<br/><br/> 3) PMS 개선:");
+			desc = desc.replace("4) 면역과민반응에", "<br/><br/> 4) 면역과민반응에");
+			desc = desc.replace("피부 상태 개선: 160~300(mg)", "피부 상태 개선: 160~300(mg)<br/><br/>");
+			desc = desc.replace("의약품의 경우 자세한 복용 방법", "· 의약품의 경우 자세한 복용 방법");
+			desc = desc.replace("목적에 따라 하루 권장 섭취량이 달라져요.", "· 목적에 따라 하루 권장 섭취량이 달라져요.");
+			desc = desc.replace("2) 당뇨신경병증", "<br/><br/>2) 당뇨신경병증");
+			desc = desc.replace("· 의약품으로는 하루 50~300(mg)이", "<br/><br/> · 의약품으로는 하루 50~300(mg)이");
+			desc = desc.replace("건강기능식품으로 인정받은 녹차추출물은 주요 지표물질로 카테킨이 20% 이상 들어있어요.", "· 건강기능식품으로 인정받은 녹차추출물은 주요 지표물질로 카테킨이 20% 이상 들어있어요.");
+	        desc = desc.replace("· 관련 연구에 따르면 하루 100~400(mg)", "<br/><br/>· 관련 연구에 따르면 하루 100~400(mg)");
+	        desc = desc.replace("2) 인지질 중 포스파티딜콜린", "<br/><br/>2) 인지질 중 포스파티딜콜린");
+	        desc = desc.replace("3) 레시틴으로서 1200~1800(mg)","<br/><br/>3) 레시틴으로서 1200~1800(mg)");
+	        desc = desc.replace("2) 나린진(Naringin): 18.78(mg)", "<br/><br/>2) 나린진(Naringin): 18.78(mg)");
+	        desc = desc.replace("4:1 정도의 비율이 적절하다고 알려져 있어요.", "· 4:1 정도의 비율이 적절하다고 알려져 있어요.");
+	        desc = desc.replace("· '실리마린'이나 '실리빈' 함량이 표기되어 있는지 확인하시는 게 좋아요!", "· '실리마린'이나 '실리빈' 함량이 표기되어 있는지 확인하시는 게 좋아요!<br/><br/>");
+	        desc = desc.replace("코로솔산 함량이 표시되어 있다면", "· 코로솔산 함량이 표시되어 있다면");
+	        desc = desc.replace("1) 피부건강, 항산화: 8~150(mg)", "<br/><br/>1) 피부건강, 항산화: 8~150(mg)");
+	        desc = desc.replace("2) 혈중 콜레스테롤 개선: 40~150(mg)", "<br/><br/>2) 혈중 콜레스테롤 개선: 40~150(mg)<br/><br/>");
+	        desc = desc.replace("1) 퀘르세틴(Quercetin) 3.66(mg)", "1) 퀘르세틴(Quercetin) 3.66(mg)<br/><br/>");
+	        desc = desc.replace("2) 이소람네틴(Isorhamnetin) 0.111(mg)", "2) 이소람네틴(Isorhamnetin): 0.111(mg)<br/>");
+	        desc = desc.replace("몸에서 만들어지기도 하지만, 상황이","· 몸에서 만들어지기도 하지만, 상황이");
+	        desc = desc.replace("1) 기억력 개선: 0.9~2(g)", "1) 기억력 개선: 0.9~2(g)<br/><br/>");
+	        desc = desc.replace("2) 건조한 눈 개선: 0.6~2.24(g)", "2) 건조한 눈 개선: 0.6~2.24(g)<br/><br/>");
+	        desc = desc.replace("3) 혈액순환 개선: 0.5~2(g)", "3) 혈액순환 개선: 0.5~2(g)<br/><br/>");
+	        desc = desc.replace("4) 혈중 중성지방 개선: 0.5~2(g)", "4) 혈중 중성지방 개선: 0.5~2(g)<br/><br/>");
+	        desc = desc.replace("속이 불편할 수 있으니 충분한 식사를 하신 후에 드세요.", "· 속이 불편할 수 있으니 충분한 식사를 하신 후에 드세요.");
+	        desc = desc.replace("1) 긴장 완화: 150(mg) 이상, 알파에스1카제인으로 2.7(mg) 이상", "1) 긴장 완화: 150(mg) 이상, 알파에스1카제인으로 2.7(mg) 이상<br/><br/>");
+	        desc = desc.replace("1) 장 건강: 6.4~20(g)", "1) 장 건강: 6.4~20(g)<br/><br/>");
+	        desc = desc.replace("2) 혈중 콜레스테롤 개선: 7.2~20(g)", "2) 혈중 콜레스테롤 개선: 7.2~20(g)<br/><br/>");
+	        desc = desc.replace("목적에 따라 달라지는 하루 권장 섭취량을 확인해보세요!", "· 목적에 따라 달라지는 하루 권장 섭취량을 확인해보세요!");
+	        desc = desc.replace("1) 면역력 증진, 피로개선: 진세노사이드 Rg1과 Rb1의 합계로서 3~80(mg)", "1) 면역력 증진, 피로개선: 진세노사이드 Rg1과 Rb1의 합계로서 3~80(mg)<br/><br/>");
+	        desc = desc.replace("2) 뼈 건강 : 진세노사이드 Rg1과 Rb1의 합계로서 25(mg)", "2) 뼈 건강 : 진세노사이드 Rg1과 Rb1의 합계로서 25(mg)<br/><br/>");
+	        desc = desc.replace("1) 후코잔틴 2.7(mg) (미역추출물 주성분)", "1) 후코잔틴 2.7(mg) (미역추출물 주성분)<br/><br/>");
+	        desc = desc.replace("1) 혈중 콜레스테롤 개선: 5.5(g) 이상", "1) 혈중 콜레스테롤 개선: 5.5(g) 이상<br/><br/>");
+	        desc = desc.replace("1) 건조 분말 형태: 하루 250~1600(mg)", "1) 건조 분말 형태: 하루 250~1600(mg)<br/><br/>");
+	        desc = desc.replace("2) 추출물 형태: 하루 120~1600(mg)", "2) 추출물 형태: 하루 120~1600(mg)<br/><br/>");
+	        desc = desc.replace("3) 주스 형태: 하루 120~750(mg)", "3) 주스 형태: 하루 120~750(mg)<br/><br/>");
+	        desc = desc.replace("1) 피부건강, 항산화에 도움을 줄 수 있음 : 총 엽록소로서 8~150(mg)", "1) 피부건강, 항산화에 도움을 줄 수 있음 : 총 엽록소로서 8~150(mg)<br/><br/>");
+	        desc = desc.replace("1) 혈중 콜레스테롤 개선: 1.2~4.5(g)", "1) 혈중 콜레스테롤 개선: 1.2~4.5(g)<br/><br/>");
+	        desc = desc.replace("1) 혈중 콜레스테롤 개선: 5~20(mg)", "1) 혈중 콜레스테롤 개선: 5~20(mg)<br/><br/>");
+	        desc = desc.replace("프로시아니딘으로 표준화 되어있는지 확인해보시는 것도 좋아요!", "프로시아니딘으로 표준화 되어있는지 확인해보시는 것도 좋아요!<br/><br/>");
+	        desc = desc.replace("[피크노제놀 함량 기준]", "[피크노제놀 함량 기준]<br/><br/>");
+	        desc = desc.replace("1) 항산화(활성산소 제거): 50~200(mg)", "1) 항산화(활성산소 제거): 50~200(mg)<br/><br/>");
+	        desc = desc.replace("2) 갱년기 여성 건강: 60~200(mg)", "2) 갱년기 여성 건강: 60~200(mg)<br/><br/>");
+	        desc = desc.replace("3) 혈액순환 개선: 100~200(mg)", "3) 혈액순환 개선: 100~200(mg)<br/><br/>");
+	        desc = desc.replace("[프로시아니딘 함량 기준]", "[프로시아니딘 함량 기준]<br/><br/>");
+	        desc = desc.replace("1) 항산화(활성산소 제거): 30~160(mg)", "1) 항산화(활성산소 제거): 30~160(mg)<br/><br/>");
+	        desc = desc.replace("2) 갱년기 여성 건강: 36~160(mg)", "2) 갱년기 여성 건강: 36~160(mg)<br/><br/>");
+	        desc = desc.replace("1) 면역력 증진, 피로개선: 3~80(mg)", "<br/><br/>1) 면역력 증진, 피로개선: 3~80(mg)<br/><br/>");
+	        desc = desc.replace("2) 혈액 흐름, 기억력 개선, 항산화: 2.4~80(mg)", "2) 혈액 흐름, 기억력 개선, 항산화: 2.4~80(mg)<br/><br/>");
+	        desc = desc.replace("3) 갱년기 여성 건강 개선: 25~80(mg)", "3) 갱년기 여성 건강 개선: 25~80(mg)<br/>");
+	        
+	        
 		}
 		else {
 			desc = "";
@@ -139,9 +191,63 @@ public class NutrientSelectServiceImpl implements NutrientSelectService {
 	        caution = caution.replace("· 심장 질환이 있는 경우 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">💗</span> 심장 질환이 있는 경우 전문가와 상담이 필요해요.");
 	        caution = caution.replace("· 정신행동질환이 있는 경우", "<span style=\"font-size: 27px;\">🧠</span> 정신행동질환이 있는 경우");
 	        caution = caution.replace("· 수술 전후 혈당 수치와", "<span style=\"font-size: 27px;\">🩸</span> 수술 전후 혈당 수치와");
-	        //가르니시아
+	        caution = caution.replace("· 견과류 알러지가", "<span style=\"font-size: 27px;\">🥜</span> 견과류 알러지가");       
+	        caution = caution.replace("· 대두 알러지", "<span style=\"font-size: 27px;\">🥫</span> 대두 알러지");       
+	        caution = caution.replace("· 담낭 질환이", "<span style=\"font-size: 27px;\">🩺</span> 담낭 질환이");
+	        caution = caution.replace("· 천식 환자는", "<span style=\"font-size: 27px;\">😷</span> 천식 환자는");
+	        caution = caution.replace("· 요오드 조절에", "<span style=\"font-size: 27px;\">🩺</span> 요오드 조절에");
+	        caution = caution.replace("· 위장을 자극할 수 있으므로", "<span style=\"font-size: 27px;\">🚽</span> 위장을 자극할 수 있으므로");
+	        caution = caution.replace("· 당뇨가 있는 경우 전문가와 상담","<span style=\"font-size: 27px;\">🩺</span> 당뇨가 있는 경우 전문가와 상담");
+	        caution = caution.replace("· 카페인이 함유되어", "<span style=\"font-size: 27px;\">☕</span> 카페인이 함유되어");
+	        caution = caution.replace("· 혈당에 영향을 줄 수 있으므로", "<span style=\"font-size: 27px;\">🩺</span> 혈당에 영향을 줄 수 있으므로");
+	        caution = caution.replace("· 혈압을 높일 수 있으므로", "<span style=\"font-size: 27px;\">🩸</span> 혈압을 높일 수 있으므로");
+	        caution = caution.replace("· 안압을 높일 수 있어", "<span style=\"font-size: 27px;\">👀</span> 안압을 높일 수 있어");
+	        caution = caution.replace("· 고지혈증약과 함께", "<span style=\"font-size: 27px;\">🩺</span> 고지혈증약과 함께");
+	        caution = caution.replace("· 카페인이 모유를","<span style=\"font-size: 27px;\">👶</span> 카페인이 모유를");
+	        caution = caution.replace("· 카페인이 혈액을 만드는","<span style=\"font-size: 27px;\">🩸</span> 카페인이 혈액을 만드는");
+	        caution = caution.replace("· 카페인으로 인해 불안 증상을","<span style=\"font-size: 27px;\">😖</span> 카페인으로 인해 불안 증상을");
+	        caution = caution.replace("· 고용량 섭취하면 발작 증상이 나타날 수 있으므로 뇌전증 환자는 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🧠</span> 고용량 섭취하면 발작 증상이 나타날 수 있으므로 뇌전증 환자는 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 카페인으로 인해 안압이 높아질","<span style=\"font-size: 27px;\">👀</span> 카페인으로 인해 안압이 높아질");
+	        caution = caution.replace("· 고용량 섭취하면 설사를","<span style=\"font-size: 27px;\">🚽</span> 고용량 섭취하면 설사를");
+	        caution = caution.replace("· 카페인으로 인해 칼슘 배설","<span style=\"font-size: 27px;\">🚽</span> 카페인으로 인해 칼슘 배설");
+	        caution = caution.replace("· 카페인을 과도하게 섭취하면 심장 박동이","<span style=\"font-size: 27px;\">💗</span> 카페인을 과도하게 섭취하면 심장 박동이");
+	        caution = caution.replace("· 여성호르몬 농도에 영향을","<span style=\"font-size: 27px;\">👩</span> 여성호르몬 농도에 영향을");
+	        caution = caution.replace("· 고용량 섭취하면 신장", "<span style=\"font-size: 27px;\">🚽</span> 고용량 섭취하면 신장");
+	        caution = caution.replace("· 고용량 섭취하면 요오드 조절에", "<span style=\"font-size: 27px;\">🩺</span> 고용량 섭취하면 요오드 조절에");
+	        caution = caution.replace("· 수술 전후 약물 대사에 영향을 줄 수 있어요.", "<span style=\"font-size: 27px;\">💊</span> 수술 전후 약물 대사에 영향을 줄 수 있어요.");
+	        caution = caution.replace("· 장기간 고용량 복용시 흡연자 폐암 위험이 증가할 수 있어요.", "<span style=\"font-size: 27px;\">🚬</span> 장기간 고용량 복용시 흡연자 폐암 위험이 증가할 수 있어요.");
+	        caution = caution.replace("· 마리골드 꽃을 원료로 하는 경우,", "<span style=\"font-size: 27px;\">🌼</span> 마리골드 꽃을 원료로 하는 경우,");
+	        caution = caution.replace("· 통풍 환자는 증상이 악화될 수 있으므로 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🩺</span> 통풍 환자는 증상이 악화될 수 있으므로 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 임산부 대상 연구가 많지 않으나,", "<span style=\"font-size: 27px;\">🤰</span> 임산부 대상 연구가 많지 않으나,");
+	        caution = caution.replace("· 국화과 식물 알러지", "<span style=\"font-size: 27px;\">🌼</span> 국화과 식물 알러지");
+	        caution = caution.replace("· 혈당 조절에 영향을 줄 수 있으므로 수술 전후", "<span style=\"font-size: 27px;\">🩺</span> 혈당 조절에 영향을 줄 수 있으므로 수술 전후");
+	        caution = caution.replace("· 자가면역질환이 있는 경우 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🩺</span> 자가면역질환이 있는 경우 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 혈압이 너무 낮아질 수 있으므로", "<span style=\"font-size: 27px;\">🩸</span> 혈압이 너무 낮아질 수 있으므로");
+	        caution = caution.replace("· 중증 간질환이 있는 경우 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🩺</span> 중증 간질환이 있는 경우 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 헤르페스 감염증을 악화시킬 수 있으므로 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🩺</span> 헤르페스 감염증을 악화시킬 수 있으므로 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 중증 간 질환이 있는 경우", "<span style=\"font-size: 27px;\">🩺</span> 중증 간 질환이 있는 경우");
+	        caution = caution.replace("· 아스피린과 유사한 영향을 미칠 수", "<span style=\"font-size: 27px;\">💊</span> 아스피린과 유사한 영향을 미칠 수");
+	        caution = caution.replace("· 유제품 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🥛</span> 유제품 알러지 주의해야해요.");
+	        caution = caution.replace("· 출산 시 혈액 응고에", "<span style=\"font-size: 27px;\">🤰</span> 출산 시 혈액 응고에");
+	        caution = caution.replace("· 임신 준비 중에는 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🤰</span> 임신 준비 중에는 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 인삼/홍삼 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🌱</span> 인삼/홍삼 알러지를 주의해야해요.");
+	        caution = caution.replace("· 태아의 건강에 영향을 줄 수 있으므로 섭취 주의해야해요.", "<span style=\"font-size: 27px;\">👶</span> 태아의 건강에 영향을 줄 수 있으므로 섭취 주의해야해요.");
+	        caution = caution.replace("· 고용량 섭취하면 수면을 방해할", "<span style=\"font-size: 27px;\">😴</span> 고용량 섭취하면 수면을 방해할");
+	        caution = caution.replace("· 수면을 방해하여 증상을 악화시킬 수 있으므로 정신 질환이", "<span style=\"font-size: 27px;\">🧠</span> 수면을 방해하여 증상을 악화시킬 수 있으므로 정신 질환이");
+	        caution = caution.replace("· 석류 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🩹</span> 석류 알러지 주의해야해요.");
+	        caution = caution.replace("· 임산부와 태아 건강에 영향을 줄 수 있으므로 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🤰</span> 임산부와 태아 건강에 영향을 줄 수 있으므로 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 간 질환이 있는 경우 장기간", "<span style=\"font-size: 27px;\">🩺</span> 간 질환이 있는 경우 장기간");
+	        caution = caution.replace("· 전립선 암에 악영향을 줄 수 있다는 우려가 있으므로", "<span style=\"font-size: 27px;\">🧬</span> 전립선 암에 악영향을 줄 수 있다는 우려가 있으므로");
+	        caution = caution.replace("· 혈압에 영향을 줄 수 있으므로 혈압 조절에 민감한 경우 주의해야해요.", "<span style=\"font-size: 27px;\">🩸</span> 혈압에 영향을 줄 수 있으므로 혈압 조절에 민감한 경우 주의해야해요.");
+	        caution = caution.replace("· 질환이 있거나 항생제 복용 시 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">💊</span> 질환이 있거나 항생제 복용 시 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 알레르기 체질은 개인에 따라 과민반응을 나타낼 수 있어요.", "<span style=\"font-size: 27px;\">🩹</span> 알레르기 체질은 개인에 따라 과민반응을 나타낼 수 있어요.");
+	        caution = caution.replace("· 꽃가루 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🌼</span> 꽃가루 알러지 주의해야해요.");
+	        caution = caution.replace("· 꿀 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🍯</span> 꿀 알러지 주의해야해요.");
+	        caution = caution.replace("· 벌 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🐝</span> 벌 알러지 주의해야해요.");
+	        caution = caution.replace("· 유당불내증 주의해야해요.", "<span style=\"font-size: 27px;\">🥛</span> 유당불내증 주의해야해요.");
+	        caution = caution.replace("· 간 질환이 있는 경우 전문가와 상담이 필요해요.", "<span style=\"font-size: 27px;\">🩺</span> 간 질환이 있는 경우 전문가와 상담이 필요해요.");
+	        caution = caution.replace("· 히알루론산 알러지 주의해야해요.", "<span style=\"font-size: 27px;\">🩹</span> 히알루론산 알러지 주의해야해요.");
 	        
-	        	        
 	        
 	    } 
 	    else {
