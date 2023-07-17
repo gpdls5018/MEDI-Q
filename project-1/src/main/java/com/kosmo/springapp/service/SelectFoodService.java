9e4dd7eb57ce46f62024fc1a348971fa929d39fc
Kimtaehyun <xogus3308@naver.com>
2023-07-17 16:36:43 +0900

    0717-조회순으로 인기검색어지정 및 검색렉 축소

--- project-1/src/main/java/com/kosmo/springapp/service/SelectFoodService.java
+++ project-1/src/main/java/com/kosmo/springapp/service/SelectFoodService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.kosmo.springapp.model.AvgStarScoreCountDTO;
+import com.kosmo.springapp.model.CheckWordDTO;
 import com.kosmo.springapp.model.FunctionalFoodListDTO;
 
 @Service
@@ -12,4 +13,8 @@ public interface SelectFoodService {
 	List<AvgStarScoreCountDTO> selectFoodListFromNameScore(String foodname);
 	List<FunctionalFoodListDTO> selectFoodListFromName(String foodname);
 	List<AvgStarScoreCountDTO> selectFoodListFoodScore();
+	List<String> RankingKeyWord();
+	List<CheckWordDTO> checkWord(String foodname);
+	void newWord(String foodname);
+	void selectKeyWord(String foodname);
 }
