c1c65cc12d3116a78b426fd44e25986783384b08
minju <betcha710@naver.com>
2023-07-20 18:37:58 +0900

    0720 - 기존 MagazineController에 병합
    - 새로 생성한 폴더 및 Controller 삭제
     (기존의 Controller 찾음)
    - 쿼리문 소량 작성

--- project-1/src/main/java/com/kosmo/springapp/controller/MagazineController.java
+++ project-1/src/main/java/com/kosmo/springapp/controller/MagazineController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kosmo.springapp.magazine.service.impl.MagazineSelectMapper;
 import com.kosmo.springapp.model.FunctionalFoodListDTO;
 import com.kosmo.springapp.service.impl.MainPageServiceImpl;
 
@@ -25,9 +26,56 @@ public class MagazineController {
 	    model.addAttribute("listData",listData);
 	    return "slider/Magazine"+no;
 	  }
-	
+	/*
 	@GetMapping("/magazineindex.do")
 	  public String magazineinddex(Map map) {
 	    return "slider/MagazineIndex";
 	  }
-}
+	*/
+	@Autowired(required=false)
+	private MagazineSelectMapper magazineSelectMapper;
+	
+	//전체 카테고리로 이동
+	@GetMapping("/magazineindex.do")
+	public String magazineSelect(Map map, Model model) {
+		
+		List<String> titleCommonSense = magazineSelectMapper.getTitleCommonSense();
+		model.addAttribute("titleCommonSense", titleCommonSense);
+		List<String> titleNutrient = magazineSelectMapper.getTitleNutrient();
+		model.addAttribute("titleNutrient", titleNutrient);
+		List<String> titleRecipe = magazineSelectMapper.getTitleRecipe();
+		model.addAttribute("titleRecipe", titleRecipe);
+		List<String> titleTrue = magazineSelectMapper.getTitleTrue();
+		model.addAttribute("titleTrue", titleTrue);
+		List<String> titleHealth = magazineSelectMapper.getTitleHealth();
+		model.addAttribute("titleHealth", titleHealth);
+		//조회수에 따른 Top6 가져오기
+		//List<String> top6 = magazineSelectMapper.getTop6();
+		
+		return "slider/MagazineIndex";
+	}////////magazineSelect
+
+	
+	/*
+	//영양제 상식 카테고리로 이동
+	@GetMapping("/magazineCommonSense.do")
+	public String magazineSelectCommonSense(Model model) {
+
+	}////////magazineSelectCommonSense
+	*/
+	
+	//영양소 A to Z 카테고리로 이동
+	
+	
+	
+	//영양제 레시피 카테고리로 이동
+	
+	
+	
+	//영양제 팩트체크 카테고리로 이동
+	
+	
+	
+	//Health & Life 카테고리로 이동
+	
+}///////////////////////////
--- project-1/src/main/java/com/kosmo/springapp/magazine/service/impl/MagazineSelectMapper.java
+++ project-1/src/main/java/com/kosmo/springapp/magazine/service/impl/MagazineSelectMapper.java
@@ -2,19 +2,28 @@ package com.kosmo.springapp.magazine.service.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MagazineSelectMapper {
 
-	// '영양소 A to Z' 부제목 가져오는 쿼리
-	List<String> getMagazineTitle();
+	List<String> getTitleCommonSense();
+
+	List<String> getTitleNutrient();
+
+	List<String> getTitleRecipe();
+
+	List<String> getTitleTrue();
+
+	List<String> getTitleHealth();
 	
-	// 기타 부제목 가져오는 쿼리
-	List<String> getETCTitle();
-	
-	// 매거진 내용에 포함된 영양제 찾아오는 쿼리
-	List<String> getIngredientName();
 	
 	// 조회수로 Top6 가져오기
 	List<String> getTop6();
 
+
+
+	
+	
 
 }/////////////////////////////////
