36f7734ca6fbcdc7fe8f438039035791dfbf4714
minju <betcha710@naver.com>
2023-07-20 11:01:44 +0900

    0720 - mapper 수정

--- project-1/src/main/java/com/kosmo/springapp/magazine/controller/MagazineController.java
+++ project-1/src/main/java/com/kosmo/springapp/magazine/controller/MagazineController.java
@@ -1,7 +1,11 @@
 package com.kosmo.springapp.magazine.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kosmo.springapp.magazine.service.impl.MagazineSelectMapper;
 
@@ -10,4 +14,23 @@ public class MagazineController {
 	
 	@Autowired
 	private MagazineSelectMapper magazineSelectMapper;
-}
+	
+	//전체 카테고리로 이동
+	@GetMapping("/magazine.do")
+	public String magazineSelect(Model model) {
+		
+		List<String> magazineTitles = magazineSelectMapper.getMagazineTitle();
+		
+		List<String> etcTitles = magazineSelectMapper.getETCTitle();
+		
+		List<String> ingredientNames = magazineSelectMapper.getIngredientName();
+		
+		//조회수에 따른 Top6 가져오기
+		List<String> top6 = magazineSelectMapper.getTop6();
+		
+		return "slider/MagazineIndex";
+	}////////magazineSelect
+	
+	
+	
+}///////////////////
--- project-1/src/main/java/com/kosmo/springapp/magazine/service/impl/MagazineSelectMapper.java
+++ project-1/src/main/java/com/kosmo/springapp/magazine/service/impl/MagazineSelectMapper.java
@@ -1,5 +1,20 @@
 package com.kosmo.springapp.magazine.service.impl;
 
-public class MagazineSelectMapper {
+import java.util.List;
 
-}
+public interface MagazineSelectMapper {
+
+	// '영양소 A to Z' 부제목 가져오는 쿼리
+	List<String> getMagazineTitle();
+	
+	// 기타 부제목 가져오는 쿼리
+	List<String> getETCTitle();
+	
+	// 매거진 내용에 포함된 영양제 찾아오는 쿼리
+	List<String> getIngredientName();
+	
+	// 조회수로 Top6 가져오기
+	List<String> getTop6();
+
+
+}/////////////////////////////////
